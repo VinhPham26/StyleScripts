b0a83079df0a6165e7a69210dfdae7a8295a84c7
Vinh Phạm <45289894+VinhPham26@users.noreply.github.com>
2024-06-29 22:46:14 +0700

    Update and rename Plex.user.js to Plex/Plex.user.js

--- Plex/Plex.user.js
+++ Plex/Plex.user.js
@@ -5,6 +5,7 @@
 // @description  Custom some style for Plex TV Web Client
 // @author       Thea
 // @match        http://192.168.1.6:32400/*
+// @match        https://app.plex.tv/desktop/*
 // @grant        none
 // @run-at       document-end
 // ==/UserScript==
@@ -19,8 +20,11 @@
         style.innerHTML = `
             /* Reduce left side bar size */
             .SourceSidebar-openSidebar-I364_Z {
-                width: 160px !important;
-                min-width: 160px !important;
+                width: 7.5% !important;
+                min-width: 7.5% !important;
+            }
+            .SourceSidebar-pane-mmshLu {
+                width: 100% !important;
             }
 
             /* Change alphabet jumpbar at left to the top */
@@ -29,17 +33,38 @@
             }
             .DirectoryListJumpBar-jumpBar-herK_R{
                 flex-flow: row !important;
-                padding: 10px 500px !important;
-                font-size: 15px !important;
-                margin: -45px 0 0 0 !important;
+                padding: 20px 20% !important;
+                font-size: 18px !important;
+                margin: -60px 0 0 0 !important;
+            }
+
+            /* Change edit metadata modal bigger */
+            .edit-metadata-modal .modal-dialog {
+                width: 40% !important;
+            }
+            .edit-metadata-modal .modal-body {
+                max-height: 70% !important;
+            }
+            .edit-metadata-modal .modal-body-pane {
+                height: 70% !important;
+            }
+            .edit-metadata-modal .selectize-dropdown-content {
+                max-height: 338px !important;
             }
         `;
         document.head.appendChild(style);
     }
 
+    function checkIsAlbum(){
+        return document.getElementsByClassName('SelectedMenuItem-menuLabel-nQZtur')[1].innerText === 'Albums';
+    }
+
     // Apply the style when the DOM is fully loaded
     if (document.readyState === 'complete') {
         applyStyle();
+        if(checkIsAlbum()){
+            alert('test')
+        }
     } else {
         window.addEventListener('load', applyStyle);
     }
