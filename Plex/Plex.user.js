153a9af9169eedb067c69f87d761f3bdca5c80c7
Vinh Phạm <45289894+VinhPham26@users.noreply.github.com>
2024-06-30 22:28:47 +0700

    Update Plex.user.js

--- Plex/Plex.user.js
+++ Plex/Plex.user.js
@@ -1,13 +1,17 @@
 // ==UserScript==
 // @name         Plex Web Style Custom
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      30/06/2024
 // @description  Custom some style for Plex TV Web Client
-// @author       Thea
+// @author       VinhPham26
+
 // @match        http://192.168.1.6:32400/*
 // @match        https://app.plex.tv/desktop/*
+
 // @grant        none
-// @run-at       document-end
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=plex.tv
+// @downloadURL  https://github.com/VinhPham26/StyleScripts/raw/main/Plex/Plex.user.js
+// @updateURL    https://github.com/VinhPham26/StyleScripts/raw/main/Plex/Plex.user.js
 // ==/UserScript==
 
 (function() {
@@ -40,16 +44,16 @@
 
             /* Change edit metadata modal bigger */
             .edit-metadata-modal .modal-dialog {
-                width: 40% !important;
+                width: 37% !important;
             }
             .edit-metadata-modal .modal-body {
-                max-height: 70% !important;
+                min-height: 700px !important;
             }
             .edit-metadata-modal .modal-body-pane {
-                height: 70% !important;
+                height: 700px !important;
             }
             .edit-metadata-modal .selectize-dropdown-content {
-                max-height: 338px !important;
+                max-height: 300px !important;
             }
         `;
         document.head.appendChild(style);
